c611821898faf0c1ef8dbf713534ba8923d38242
sethkibet <114141320+sethkibet@users.noreply.github.com>
2022-10-06 08:59:56 +0300

    Add files via upload
    odd numbers from 1 to 50

---
+++ odd numbers.cpp
@@ -0,0 +1,14 @@
+// C++ Program to print odd numbers from 1 to 50
+#include <iostream>
+using namespace std;
+int main() {
+ int i = 1;
+ // do...while loop for odd numbers from 1 to 50
+ do {
+ cout << i << " ";
+   ++i; i=i+1;
+ }
+ while (i <= 50);
+
+ return 0;
+}
