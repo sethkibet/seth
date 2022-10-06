5e9c164e95784bfe9693aa265eb1d258c091012a
sethkibet <114141320+sethkibet@users.noreply.github.com>
2022-10-06 08:41:17 +0300

    Add files via upload
    do...while loop descending

---
+++ do...while loop.cpp
@@ -0,0 +1,14 @@
+// C++ Program to print numbers from 20 to 1
+#include <iostream>
+using namespace std;
+int main() {
+ int i = 20;
+ // do...while loop from 20 to 1
+ do {
+ cout << i << " ";
+ --i;
+ }
+ while (i >= 1);
+
+ return 0;
+}
